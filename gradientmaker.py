0e50a3fbefa51a93f0f8a1b5ec70376a3bfb5315
TheAI-1 <92941724+TheAI-1@users.noreply.github.com>
2024-01-10 19:01:48 +0000

    When I wrote this, only god and I knew how it worked.
    So i added comments lol

--- gradientmaker.py
+++ gradientmaker.py
@@ -1,15 +1,17 @@
 import copy
 import tkinter as tk
 import random
+
+#creating the window and canvas using the tkinter library
 window = tk.Tk()
 window.geometry("1000x500")
 canvas = tk.Canvas(window, width=1000, height=500, bg="lightblue")
 canvas.grid(column=0, row=0)
 
 #input values
-colour1 = [185, 66, 66]
-colour2 = [10, 110, 0]
-resolution = 1000
+colour1 = [0, 191, 172]
+colour2 = [255, 33, 170]
+resolution = 5
 point = 0.3
 
 #gradient maker
@@ -17,22 +19,22 @@ difference = []
 increases = []
 resolution -= 1
 gradient = [colour1]
-total = copy.deepcopy(colour1)
+total = copy.deepcopy(colour1) #total was changing colour1 with just doing total = colour1 (with = it just refers to the same point in memory)
 for i in range(3):
-    difference.append(colour1[i] - colour2[i])
-    increases.append(difference[i]/resolution)
-for i in range(resolution):
+    difference.append(colour1[i] - colour2[i]) #finds the differences between the rgb values of the colours
+    increases.append(difference[i]/resolution) #divides the differences by the resolution to find the step size
+for i in range(resolution): #repeats for how many different colours you want
     temp = []
-    for j in range(3):
-        temp.append(total[j] - increases[j])
-        total[j] -= increases[j]
-    gradient.append(temp)
+    for j in range(3): #3 because 3 different values in each part of the list (rgb)
+        temp.append(total[j] - increases[j]) #adding the current value (r g or b) to a temporary array so it can be added to the gradient array
+        total[j] -= increases[j] #finds the total (not sure why i needed this)
+    gradient.append(temp) #saves the temporary value to the "gradient" array
 x = 0
 y = 0
 step = 1000/(resolution+1)
 
 
-
+#placing the gradient into the window
 for i in range(len(gradient)):
     hexgradient = []
     for j in range(3):
@@ -65,4 +67,3 @@ for i in range(3):
 
 colour = "#" + colour[0] + colour[1] + colour[2]
 canvas.create_rectangle(0, y+450, 1000, y+500, fill=colour, outline=colour)
-
